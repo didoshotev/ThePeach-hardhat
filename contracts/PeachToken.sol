8a06be50fc2ee8fc6c575d41a7c1906636a8fe5d
Amaechi Okolobi <okolobiam@gmail.com>
2022-04-03 19:20:06 +0100

    refactor

--- contracts/PeachToken.sol
+++ contracts/PeachToken.sol
@@ -4,69 +4,54 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./IJoeRouter02.sol";
 import "./IJoeFactory.sol";
 import "./NodeManager.sol";
 
-contract PeachNode is ERC20, Ownable {
+contract PeachNode is ERC20, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     uint256 private supply = 2000000;
     uint8 private _decimals = 18;
     uint256 private _tTotal = supply * (10 ** _decimals);
-    //NODE REWARD MANAGER
-    NodeManager public nodeRewardManager;
-
-    IJoeRouter02 public dexRouter;
-    address public lpPair;
-
-    // JOE ROUTER
-    // TESTNET ROUTER 0x2D99ABD9008Dc933ff5c0CD271B88309593aB921
-    // MAINNET ROUTER 0x60aE616a2155Ee3d9A68541Ba4544862310933d4
-
-    address private _routerAddress = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
-    address public deadWallet = 0x000000000000000000000000000000000000dEaD;
     
     address payable public liquidityPool;
     address payable public rewardsPool;
-    address payable public treasuryPool; //require contract maybe
-    address payable public marketingPool;
+    address payable public treasuryPool; 
+    address payable public teamPool;
 
-    uint256 public liquidityPoolFee;
-    uint256 public rewardsFee;
-    uint256 public treasuryFee;
-    uint256 public marketingFee;
-
-    uint256 public cashoutFee;
-    uint256 public totalFees;
+    uint8 public liquidityPoolFee = 1;
+    uint8 public rewardsFee = 3;
+    uint8 public treasuryFee = 4;
+    uint8 public teamFee = 2;
 
     // Track Blacklisted Addresses
     mapping(address => bool) public _isBlacklisted;
 
-    // Market Pairs
-    mapping(address => bool) public automatedMarketMakerPairs;
+    // Keeps track of which address are excluded from fee.
+    mapping (address => bool) private _isExcludedFromFee;
 
-    //Trader Joe Router Updated
-    event UpdateJoeV2Router(
-        address indexed newAddress,
-        address indexed oldAddress
-    );
+    event ExcludeAccountFromFee(address account);
+    event IncludeAccountInFee(address account);
 
-    event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
+    struct ValuesFromAmount {
+        // Amount of tokens for to transfer.
+        uint256 amount;
+        // Amount tokens charged to add to liquidity.
+        uint256 tLiquidityFee;
+        //Amount tokens charged to add to rewards.
+        uint256 tRewardsFee;
+        // Amount tokens charged to add to treasury.
+        uint256 tTreasuryFee;
+        //Amount tokens charged to add to marketing.
+        uint256 tTeamFee;
+        // Amount tokens after fees.
+        uint256 tTransferAmount;
 
-    event LiquidityWalletUpdated(
-        address indexed newLiquidityWallet,
-        address indexed oldLiquidityWallet
-    );
-
-    event SwapAndLiquify(
-        uint256 tokensSwapped,
-        uint256 ethReceived,
-        uint256 tokensIntoLiqudity
-    );
+    }
 
     constructor(
-        address _dexRouter,
         address payable _liquidityPool,
         address payable _rewardsPool,
         address payable _treasuryPool,
@@ -75,68 +60,28 @@ contract PeachNode is ERC20, Ownable {
         ERC20("PEACH NODE", "PEACH")
     {
 
+        excludeAccountFromFee(msg.sender);
+        excludeAccountFromFee(address(this));
+
         //Set Pool Addresses
         liquidityPool = _liquidityPool;
         rewardsPool = _rewardsPool;
         treasuryPool = _treasuryPool;
-        marketingPool = _marketingPool;
+        teamPool = _marketingPool;
 
         require(
             liquidityPool != address(0) && 
             rewardsPool != address(0) && 
             treasuryPool != address(0) && 
-            marketingPool != address(0),
+            teamPool != address(0),
             "LIQUIDITY/REWARDS/TREASURY/MARKETING POOL ADDRESS CANNOT BE ZERO"
         );
-
-        //SET ROUTER
-        dexRouter = IJoeRouter02(_dexRouter);
     }
 
-    //ICO Functions
-
-
-
-    // Liquidity functions
-
-    // Create AVAX / PEACH Liquidity Pair
-    function setJoeV2RouterAndCreatePair() public onlyOwner {
-        IJoeRouter02 _dexRouter = IJoeRouter02(_routerAddress);
-        lpPair = IJoeFactory(_dexRouter.factory()).createPair(
-            address(this),
-            _dexRouter.WAVAX()
-        );
-
-        dexRouter = _dexRouter;
-        _setAutomatedMarketMakerPair(lpPair, true);
-    }
-
-    //SET NODE MANAGER
-    function setNodeManagement(address nodeManagement) external onlyOwner {
-        nodeRewardManager = NodeManager(nodeManagement);
-    }
-    
-    //Update Router ADDRESS
-    function updateJoeV2RouterAddress(address newAddress) public onlyOwner {
-        require(
-            newAddress != address(dexRouter),
-            "This router address is already set"
-        );
-        emit UpdateJoeV2Router(newAddress, address(dexRouter));
-        dexRouter = IJoeRouter02(newAddress);
-        address _joeV2Pair = IJoeFactory(dexRouter.factory()).createPair(
-            address(this),
-            dexRouter.WAVAX()
-        );
-        lpPair = _joeV2Pair;
-    }
-
-    //Update Address Of Liquidity Pool
     function updateLiquidityPool(address payable pool) external onlyOwner {
         liquidityPool = pool;
     }
 
-    //Update Address Of Rewards Pooln
     function updateRewardsPool(address payable pool) external onlyOwner {
         rewardsPool = pool;
     }
@@ -144,39 +89,23 @@ contract PeachNode is ERC20, Ownable {
         treasuryPool = pool;
     }
     function updateMarketingPool(address payable pool) external onlyOwner {
-        marketingPool = pool;
+        teamPool = pool;
     }
 
-    function updateRewardsFee(uint256 value) external onlyOwner {
+    function updateRewardsFee(uint8 value) external onlyOwner {
         rewardsFee = value;
     }
 
-    function updateTreasuryFee(uint256 value) external onlyOwner {
+    function updateTreasuryFee(uint8 value) external onlyOwner {
         treasuryFee = value;
     }
 
-    function updateMarketingFee(uint256 value) external onlyOwner {
-        marketingFee = value;
+    function updateMarketingFee(uint8 value) external onlyOwner {
+        teamFee = value;
     }
 
-    function updateLiquidityFee(uint256 value) external onlyOwner {
+    function updateLiquidityFee(uint8 value) external onlyOwner {
         liquidityPoolFee = value;
-    }
-
-    function updateCashoutFee(uint256 value) external onlyOwner {
-        cashoutFee = value;
-    }
-
-    function setAutomatedMarketMakerPair(address pair, bool value)
-        public
-        onlyOwner
-    {
-        require(
-            pair != lpPair,
-            "TKN: The Trader Joe pair cannot be removed from automatedMarketMakerPairs"
-        );
-
-        _setAutomatedMarketMakerPair(pair, value);
     }
 
     function blacklistMalicious(address account, bool value)
@@ -186,213 +115,63 @@ contract PeachNode is ERC20, Ownable {
         _isBlacklisted[account] = value;
     }
 
-    function _setAutomatedMarketMakerPair(address pair, bool value) private {
-        require(
-            automatedMarketMakerPairs[pair] != value,
-            "TKN: Automated market maker pair is already set to that value"
-        );
-        automatedMarketMakerPairs[pair] = value;
+    function _transfer(address sender, address recipient, uint256 amount) internal override {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(!_isBlacklisted[sender] && !_isBlacklisted[recipient],"Blacklisted address");
+        
+        ValuesFromAmount memory values = _getValues(amount, _isExcludedFromFee[sender]);
+        
+        emit Transfer(sender, recipient, values.tTransferAmount);
+        super._transfer(sender, treasuryPool, values.amount - values.tTransferAmount);
+        
 
-        emit SetAutomatedMarketMakerPair(pair, value);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-        require(
-            !_isBlacklisted[from] && !_isBlacklisted[to],
-            "Blacklisted address"
-        );
-
-        super._transfer(from, to, amount);
+    function isExcludedFromFee(address account) external view returns (bool) {
+        return _isExcludedFromFee[account];
     }
-
-    //Swap Tokens for AVAX
-    function swapAndSendToFee(address destination, uint256 tokens) private {
-        uint256 initialAVAXBalance = address(this).balance;
-
-        swapTokensForAVAX(tokens);
-        uint256 newBalance = (address(this).balance).sub(initialAVAXBalance);
-        payable(destination).transfer(newBalance);
-    }
-
-    //Swap Half Tokens Liquify Half Tokens
-    function swapAndLiquify(uint256 tokens) private {
-        uint256 half = tokens.div(2);
-        uint256 otherHalf = tokens.sub(half);
-        uint256 initialBalance = address(this).balance;
-        swapTokensForAVAX(half);
-
-        uint256 newBalance = address(this).balance.sub(initialBalance);
-        addLiquidity(otherHalf, newBalance);
-        emit SwapAndLiquify(half, newBalance, otherHalf);
-    }
-
-    //Token to AVAX Swap
-    function swapTokensForAVAX(uint256 tokenAmount) private {
-        address[] memory path = new address[](2);
-        path[0] = address(this);
-        path[1] = dexRouter.WAVAX();
-
-        _approve(address(this), address(dexRouter), tokenAmount);
-
-        dexRouter.swapExactTokensForAVAXSupportingFeeOnTransferTokens(
-            tokenAmount,
-            0, // accept any amount of AVAX
-            path,
-            address(this),
-            block.timestamp
-        );
-    }
-
-    // Add Liquidity
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-        // approve token
-        _approve(address(this), address(dexRouter), tokenAmount);
-
-        dexRouter.addLiquidityAVAX{value: ethAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            liquidityPool,
-            block.timestamp
-        );
-    }
-
-    // Create Node
-    // function createNodeWithTokens(string memory name) public {
-    //     address sender = _msgSender();
-    //     uint256 nodePrice = nodeRewardManager.nodePrice();
-    //     require(
-    //         bytes(name).length > 3 && bytes(name).length < 32,
-    //         "NODE CREATION: NAME SIZE INVALID"
-    //     );
-    //     require(
-    //         sender != address(0),
-    //         "NODE CREATION:  creation from the zero address"
-    //     );
-    //     require(!_isBlacklisted[sender], "NODE CREATION: Blacklisted address");
-    //     require(
-    //         sender != liquidityPool && sender != rewardsPool,
-    //         "NODE CREATION: liquidityPool and rewardsPool cannot create node"
-    //     );
-    //     require(
-    //         balanceOf(sender) >= nodeRewardManager._getNodePrice(),
-    //         "NODE CREATION: Balance too low for creation."
-    //     );
-
-    //     super._transfer(sender, address(this), nodePrice);
-    //     nodeRewardManager.createNode(sender, name);
-    // }
-
-    // Claim Rewards
-    // function claimRewards() public {
-    //     address sender = _msgSender();
-    //     require(
-    //         sender != address(0),
-    //         "CASHOUT: zero address cannot cash out rewards"
-    //     );
-    //     require(
-    //         !_isBlacklisted[sender],
-    //         "CASHOUT: Blacklisted address cannot cash out rewards"
-    //     );
-    //     require(
-    //         sender != liquidityPool && sender != rewardsPool,
-    //         "CASHOUT: future and rewardsPool cannot cashout rewards"
-    //     );
-    //     uint256 rewardAmount = nodeRewardManager._getRewardAmountOf(sender);
-    //     require(
-    //         rewardAmount > 0,
-    //         "CASHOUT: You don't have enough reward to cash out"
-    //     );
-    //     super._transfer(rewardsPool, sender, rewardAmount);
-    //     nodeRewardManager._cashoutAllNodesReward(sender);
-    // }
-
-    //View Number Of Nodes For Address
-    // function getNodeNumberOf(address account) public view returns (uint256) {
-    //     return nodeRewardManager._getNodeNumberOf(account);
-    // }
-
-    //View Total Reward Amount
-    // function getRewardAmount() public view returns (uint256) {
-    //     require(msg.sender != address(0), "SENDER CAN'T BE ZERO");
-    //     require(nodeRewardManager._isNodeOwner(msg.sender), "NO NODE OWNER");
-    //     return nodeRewardManager._getRewardAmountOf(msg.sender);
-    // }
-
-
-    // function changeNodePrice(uint256 newNodePrice) public onlyOwner {
-    //     nodeRewardManager._changeNodePrice(newNodePrice);
-    // }
-
-    // function getNodePrice() public view returns (uint256) {
-    //     return nodeRewardManager._getNodePrice();
-    // }
-
-    // function changeRewardPerNode(uint256 newPrice) public onlyOwner {
-    //     nodeRewardManager._changeRewardPerNode(newPrice);
-    // }
-
-    // function getRewardPerNode() public view returns (uint256) {
-    //     return nodeRewardManager._getRewardPerNode();
-    // }
-
-    //Set Auto Distribution Of Rewards Bool
-    // function changeAutoDistri(bool newMode) public onlyOwner {
-    //     nodeRewardManager._changeAutoDistri(newMode);
-    // }
-
-    // //Change Gas For Rewards Distribution
-    // function changeGasDistri(uint256 newGasDistri) public onlyOwner {
-    //     nodeRewardManager._changeGasDistri(newGasDistri);
-    // }
-
-    //View Node Names
-    // function getNodesNames() public view returns (string memory) {
-    //     require(_msgSender() != address(0), "SENDER CAN'T BE ZERO");
-    //     require(nodeRewardManager._isNodeOwner(_msgSender()), "NO NODE OWNER");
-    //     return nodeRewardManager._getNodesNames(_msgSender());
-    // }
-
-    // //View Time Since First Node Created
-    // function getNodesCreatime() public view returns (string memory) {
-    //     require(_msgSender() != address(0), "SENDER CAN'T BE ZERO");
-    //     require(nodeRewardManager._isNodeOwner(_msgSender()), "NO NODE OWNER");
-    //     return nodeRewardManager._getNodesCreationTime(_msgSender());
-    // }
-
-    // //View Rewards Available
-    // function getNodesRewards() public view returns (string memory) {
-    //     require(_msgSender() != address(0), "SENDER CAN'T BE ZERO");
-    //     require(nodeRewardManager._isNodeOwner(_msgSender()), "NO NODE OWNER");
-    //     return nodeRewardManager._getNodesRewardAvailable(_msgSender());
-    // }
-
-    // //View Time Since Last Claim
-    // function getNodesLastClaims() public view returns (string memory) {
-    //     require(_msgSender() != address(0), "SENDER CAN'T BE ZERO");
-    //     require(nodeRewardManager._isNodeOwner(_msgSender()), "NO NODE OWNER");
-    //     return nodeRewardManager._getNodesLastClaimTime(_msgSender());
-    // }
-
-    // //Distribute Rewards To Node Owners
-    // function distributeRewards()
-    //     public
-    //     onlyOwner
-    //     returns (
-    //         uint256,
-    //         uint256,
-    //         uint256
-    //     )
-    // {
-    //     return nodeRewardManager._distributeRewards();
-    // }
     
-    
+    function excludeAccountFromFee(address account) internal {
+        require(!_isExcludedFromFee[account], "Account is already excluded.");
+
+        _isExcludedFromFee[account] = true;
+
+        emit ExcludeAccountFromFee(account);
+    }
+
+    function includeAccountInFee(address account) internal {
+        require(_isExcludedFromFee[account], "Account is already included.");
+
+        _isExcludedFromFee[account] = false;
+
+        emit IncludeAccountInFee(account);
+    }
+
+    function _getValues(uint256 amount, bool deductTransferFee) private view returns (ValuesFromAmount memory) {
+        ValuesFromAmount memory values;
+        values.amount = amount;
+        _getTValues(values, deductTransferFee);
+        return values;
+    }
+
+    function _getTValues(ValuesFromAmount memory values, bool deductTransferFee) view private {
+
+        if (deductTransferFee) {
+            values.tTransferAmount = values.amount;
+        } else {
+            // calculate fee
+            values.tRewardsFee = _calculateTax(values.amount, rewardsFee, 0);
+            values.tLiquidityFee = _calculateTax(values.amount, liquidityPoolFee, 0);
+            values.tTreasuryFee = _calculateTax(values.amount, treasuryFee, 0);
+            values.tTeamFee = _calculateTax(values.amount, teamFee, 0);
+
+            // amount after fee
+            values.tTransferAmount = values.amount - values.tRewardsFee - values.tLiquidityFee - values.tTreasuryFee - values.tTeamFee;
+        }
+    }
+
+    function _calculateTax(uint256 amount, uint8 tax, uint8 taxDecimals_) private pure returns (uint256) {
+        return amount * tax / (10 ** taxDecimals_) / (10 ** 2);
+    }
 }
